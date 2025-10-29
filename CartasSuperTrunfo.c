6e8061e8b34a5fdaa758f9f720ca9a31a6975b0d
minaro9-uzu <luanvito27@gmail.com>
2025-10-29 12:38:52 +0000

    criei duas cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,25 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  //=====carta1=====
+    char estado1 = 'S';
+    char codigocarta1[4] = "SP01";
+    char nomecidade1[8] = "sãopaulo";
+    int população1 = 12325000;
+    float areaKm21 = 1521.11;
+    float pib1 = 799000000000.0;
+    int NUmPontosTuristicos1 = 25;
 
+  //=====carta2=====
+    char estado2 = 'P';
+    char codigocarta2[4] = "PE01";
+    char nomecidade2[6] = "RECIFE";
+    int população2 = 1660000;
+    float areaKm2 = 218.84;
+    float pib2 = 66000000000.0;
+    int NUmPontosTuristicos2 = 15;
+
+    
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
